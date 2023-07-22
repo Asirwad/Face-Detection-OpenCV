093aa8655dd610bbac1f0b4444ba8a0ef49c06ad
Asirwad <asirwadsali@gmail.com>
2023-07-22 21:01:41 +0530

    init

--- app/main.py
+++ app/main.py
@@ -1,2 +1,23 @@
 import cv2
 
+try:
+    trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    webcam = cv2.VideoCapture(0)
+
+    while True:
+        is_frame_read_success, frame = webcam.read()
+        grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        face_coordinates = trained_face_data.detectMultiScale(frame)
+        print(face_coordinates)
+        for coordinate in face_coordinates:
+            (x, y, w, h) = coordinate
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.imshow("", frame)
+        key = cv2.waitKey(1)
+
+        # if q is pressed , exit
+        if key in (81, 113):
+            break
+
+except Exception as e:
+    print(e)
