d61bf53014ea200e9c29935a082dcf08807ea6ff
Asirwad <asirwadsali@gmail.com>
2023-07-23 12:48:54 +0530

    added smile detection to CLI script

--- app/main_CLI.py
+++ app/main_CLI.py
@@ -2,16 +2,24 @@ import cv2
 
 try:
     trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    trained_smile_data = cv2.CascadeClassifier('haarcascade_smile.xml')
     webcam = cv2.VideoCapture(0)
 
     while True:
         is_frame_read_success, frame = webcam.read()
         grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         face_coordinates = trained_face_data.detectMultiScale(grey_frame)
-        print(face_coordinates)
+        smile_coordinates = trained_smile_data.detectMultiScale(grey_frame,
+                                                                scaleFactor=1.7,
+                                                                minNeighbors=20)
+        print(f"face_coordinates : {face_coordinates}")
+        print(f"smile coordinates {smile_coordinates}")
         for coordinate in face_coordinates:
             (x, y, w, h) = coordinate
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 4)
+        for coordinate in smile_coordinates:
+            (x, y, w, h) = coordinate
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 4)
         cv2.imshow("", frame)
         key = cv2.waitKey(1)
 
